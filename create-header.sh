c61896eb1caba18c9dc91118056799ffaf0b4306
Michael Hoffer <info@michaelhoffer.de>
2010-10-16 21:24:53 +0200

    create-header.sh:q

--- create-header.sh
+++ create-header.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 source ./PROJECT_PATH
+echo -e ">> compiling project:"
 
-echo -e ">> creating header file: VRL-UG4.h"
-javah -o VRL-UG4.h -classpath VRL-UG4/build/classes edu.gcsc.vrl.ug4.UG4
-echo -e ">> copying header file \"VRL-UG4.h\" to \"$UG4/bindings_vrl/\" folder"
-cp VRL-UG4.h $UG4/ugbase/bindings_vrl/
+CWD="$(pwd)"
+
+cd VRL-UG4
+
+ant clean
+ant jar
+
+cd "$CWD"
+
+echo -e ">> creating header file:"
+javah -o bindings_vrl_native.h -classpath VRL-UG4/build/classes edu.gcsc.vrl.ug4.UG4
+echo -e ">> copying header file to \"$UG4/bindings_vrl/\" folder"
+cp bindings_vrl_native.h $UG4/ugbase/bindings_vrl/
