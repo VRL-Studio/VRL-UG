d453eb5e7fb9a22e972d28860a339b00670b80dc
Michael Hoffer <info@michaelhoffer.de>
2011-02-04 20:13:48 +0100

    header generation extended for memmanager

--- create-header.sh
+++ create-header.sh
@@ -13,6 +13,6 @@ ant jar
 cd "$CWD"
 
 echo -e ">> creating header file:"
-javah -o bindings_vrl_native.h -classpath VRL-UG4/build/classes edu.gcsc.vrl.ug4.UG4
+javah -o bindings_vrl_native.h -classpath VRL-UG4/build/classes edu.gcsc.vrl.ug4.UG4 edu.gcsc.vrl.ug4.MemoryManager
 echo -e ">> copying header file to \"$UG4/bindings_vrl/\" folder"
 cp bindings_vrl_native.h $UG4/ugbase/bindings_vrl/
