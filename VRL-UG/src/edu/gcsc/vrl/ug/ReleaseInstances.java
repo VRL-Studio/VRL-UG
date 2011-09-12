21fea9195e56a812658612297f253e2341f50c8f
Michael Hoffer <info@michaelhoffer.de>
2011-09-12 17:17:21 +0200

    allow release of ug instances

---
+++ VRL-UG/src/edu/gcsc/vrl/ug/ReleaseInstances.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.gcsc.vrl.ug;
+
+import eu.mihosoft.vrl.annotation.ComponentInfo;
+import eu.mihosoft.vrl.annotation.MethodInfo;
+import eu.mihosoft.vrl.annotation.ObjectInfo;
+import eu.mihosoft.vrl.reflection.VisualCanvas;
+import eu.mihosoft.vrl.types.CanvasRequest;
+
+/**
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+@ComponentInfo(name = "Release Instances", category = "UG4/Util")
+@ObjectInfo(name="Release Instances")
+public class ReleaseInstances {
+
+    @MethodInfo()
+    public void release(CanvasRequest cReq) {
+        if (cReq != null) {
+            MemoryManager.releaseAll(cReq.getCanvas());
+        }
+    }
+
+    @MethodInfo(noGUI=true)
+    public void release(VisualCanvas canvas) {
+        MemoryManager.releaseAll(canvas);
+    }
+}
