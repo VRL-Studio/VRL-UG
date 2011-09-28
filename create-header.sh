757aeacabd770aa91b633cb98375fbb8936e82f3
Michael Hoffer <info@michaelhoffer.de>
2011-09-28 18:13:52 +0200

    paths adjusted due to ug related refactoring

--- create-header.sh
+++ create-header.sh
@@ -14,5 +14,5 @@ cd "$CWD"
 
 echo -e ">> creating header file:"
 javah -o bindings_vrl_native.h -classpath VRL-UG/build/classes edu.gcsc.vrl.ug.UG edu.gcsc.vrl.ug.MemoryManager
-echo -e ">> copying header file to \"$UG4/bindings_vrl/\" folder"
-cp bindings_vrl_native.h $UG4/ugbase/bindings_vrl/
+echo -e ">> copying header file to \"$UG4/bindings/vrl/\" folder"
+cp bindings_vrl_native.h $UG4/ugbase/bindings/vrl/
