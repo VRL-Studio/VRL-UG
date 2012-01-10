0383d1e83c251466b3769404f69396939544dd95
Christian Poliwoda <christian.poliwoda@gcsc.uni-frankfurt.de>
2012-01-10 13:44:15 +0100

    start to integrate commandLineOptions in Main.java to start automativ a second JVM from client side

--- VRL-UG/src/edu/gcsc/vrl/ug/Main.java
+++ VRL-UG/src/edu/gcsc/vrl/ug/Main.java
@@ -5,6 +5,12 @@
 package edu.gcsc.vrl.ug;
 
 import eu.mihosoft.vrl.system.VRL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,9 +18,100 @@ import eu.mihosoft.vrl.system.VRL;
  */
 public class Main {
 
-    private static Integer defaultPort = 1099; // default port
-
+    private static Integer port = 1099; // default port
+    
     public static void main(String[] args) {
+        
+        initUGServer();
+        
+    }
+
+    
+       protected static void startAnJVM(int port) {
+        
+        String commandLineCallOptions = "-Xms256m -Xmx1024m "
+                + "-XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled "
+                + "-XX:+CMSPermGenSweepingEnabled -XX:MaxPermSize=256m";
+        
+        String addionalCommandLineCallPath = 
+        "-Dsun.boot.library.path=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Libraries"
+                + ":natives/osx:custom-lib/osx"
+                + " -Xbootclasspath/p"
+                + ":natives/jars/j3dcore.jar"
+                + ":natives/jars/j3dutils.jar"
+                + ":natives/osx/jogl.jar"
+                + ":natives/jars/vecmath.jar"
+                + ":natives/osx/gluegen-rt.jar"
+                + " -Djava.library.path=\"natives/osx\"";
+        
+        String separator = System.getProperty("file.separator");
+        String classpath = System.getProperty("java.class.path");
+        String path = System.getProperty("java.home")
+                + separator + "bin" + separator + "java";
+        
+        
+        System.out.println("separator = " + separator);
+        System.out.println("classpath = " + classpath);
+        System.out.println("path = " + path);
+        
+
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                path, "-cp", classpath, Main.class.getName() );
+
+        
+        processBuilder.redirectErrorStream(true);//NEEDED TO READ / VIEW OUTPUT OF 2nd JVM
+
+        Process process = null;
+        try {
+            process = processBuilder.start();
+        } catch (IOException ex) {
+            Logger.getLogger(UG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        //NEEDED TO READ / VIEW OUTPUT OF 2nd JVM
+        try {
+
+            // hook up child process output to parent
+            InputStream inStream = process.getInputStream();
+            InputStreamReader inStreamRead = new InputStreamReader(inStream);
+            BufferedReader buffInStreamRead = new BufferedReader(inStreamRead);
+
+            // hook up child process error output to parent
+            InputStream errStream = process.getErrorStream();
+            InputStreamReader errStreamRead = new InputStreamReader(errStream);
+            BufferedReader buffErrStreamRead = new BufferedReader(errStreamRead);
+
+            
+            // read the child process' output
+            String line;
+            while ((line = buffInStreamRead.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+            String line2;
+            // read the child process' error output
+            while ((line2 = buffErrStreamRead.readLine()) != null) {
+                System.err.println(line2);
+            }
+
+        } catch (Exception e) { // exception thrown
+
+            System.out.println("ERROR during reading stream of Server JVM !");
+
+        }
+        
+        try {
+            
+            process.waitFor();
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(UG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    protected static void initUGServer() {
 
         String[] params = {"-property-folder-suffix", "numerics-server",
             "-plugin-checksum-test", "yes", "-rpc", "server"};
@@ -25,11 +122,17 @@ public class Main {
 //
 //            if (args[0] != null) {
 //                Integer tmp = Integer.parseInt(args[0]);
-
+//
 //                // check port range between min and max
 //                if (0 <= tmp && tmp <= 65535) {
-//                    defaultPort = tmp;
+//                    port = tmp;
 //                }
+//            }
+//        }
+//        
+//        UG.createXmlRpcServer(port);
+        
+        
 
 //                if (tmp == 1) {
 
@@ -48,8 +151,6 @@ public class Main {
 //                    VRL.initAll(args);
 //                    UG.setRemoteType(RemoteType.NONE);
 //                }
-//            }
-//        }
 
     }
 }
