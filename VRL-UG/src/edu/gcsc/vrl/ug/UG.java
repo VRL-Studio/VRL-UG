e765962389f51eb55821cf37d75db27b655d86a0
Michael Hoffer <info@michaelhoffer.de>
2012-01-22 20:56:03 +0100

    rewrote library loading functionality. library dependencies are detected now!

--- VRL-UG/src/edu/gcsc/vrl/ug/UG.java
+++ VRL-UG/src/edu/gcsc/vrl/ug/UG.java
@@ -4,40 +4,38 @@
  */
 package edu.gcsc.vrl.ug;
 
+import eu.mihosoft.vrl.io.IOUtil;
 import eu.mihosoft.vrl.io.VJarUtil;
 import eu.mihosoft.vrl.io.VPropertyFolderManager;
 import eu.mihosoft.vrl.reflection.VisualCanvas;
-import eu.mihosoft.vrl.system.Constants;
-import eu.mihosoft.vrl.system.VRL;
-import eu.mihosoft.vrl.system.VSysUtil;
-import eu.mihosoft.vrl.system.VTerminalUtil;
+import eu.mihosoft.vrl.system.*;
 import eu.mihosoft.vrl.visual.MessageType;
 import eu.mihosoft.vrl.visual.SplashScreenGenerator;
 import java.beans.XMLDecoder;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 
 /**
- * <p>
- * UG class represents UG and its scripting functionality. It allows only one
- * instance which can be obtained via
- * {@link #getInstance(eu.mihosoft.vrl.reflection.VisualCanvas) }.
- * </p>
- * <p>
- * <b>Note:</b> this singleton must not be loaded by more than one
- * classloader per JVM! Although this is no problem for Java classes, it
- * does not work for native libraries. Unfortunately, we cannot provide an
- * acceptable workaround. Thus, it is recommended to use this class from
- * a valid VRL plugin only. The VRL plugin system is aware of this problem
- * and handles it correctly.
- * </p>
+ * <p> UG class represents UG and its scripting functionality. It allows only
+ * one instance which can be obtained via
+ * {@link #getInstance(eu.mihosoft.vrl.reflection.VisualCanvas) }. </p> <p>
+ * <b>Note:</b> this singleton must not be loaded by more than one classloader
+ * per JVM! Although this is no problem for Java classes, it does not work for
+ * native libraries. Unfortunately, we cannot provide an acceptable workaround.
+ * Thus, it is recommended to use this class from a valid VRL plugin only. The
+ * VRL plugin system is aware of this problem and handles it correctly. </p>
+ *
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
 public class UG {
@@ -83,8 +81,8 @@ public class UG {
      */
     private static boolean libLoaded = false;
     /**
-     * temp folder conaining native libs 
-     * (must be set from ug plugin, not from api)
+     * temp folder conaining native libs (must be set from ug plugin, not from
+     * api)
      */
     private static File nativeLibFolder;
     /**
@@ -94,6 +92,7 @@ public class UG {
 
     /**
      * Returns the api description
+     *
      * @return api description
      */
     public String getDescriptionFromApi() {
@@ -134,8 +133,79 @@ public class UG {
 //    }
 //
     /**
+     * Loads all native librarties in the specified folder and optionally all
+     * of its subfolders. Please ensure that all libraries in the folder are
+     * compatible with the current os.
+     * <p><b>Note: </b>this method is an EXACT copy of 
+     * {@link eu.mihosoft.vrl.system.VSysUtil#loadNativeLibrariesInFolder(java.io.File, boolean) }. 
+     * This is necessary as using the original method would load the native 
+     * libraries in the wrong classloader (the classloader that loaded
+     * {@link eu.mihosoft.vrl.system.VSysUtil)}.
+     * </p>
+     *
+     * @param folder library folder
+     * @param recursive defines whether recusrively load libraries from sub
+     * folders
+     *
+     * @return
+     * <code>true</code> if all native libraries could be loaded;
+     * <code>false</code> otherwise
+     */
+    private static boolean loadNativeLibrariesInFolder(
+            File folder, boolean recursive) {
+        VParamUtil.throwIfNotValid(
+                VParamUtil.VALIDATOR_EXISTING_FOLDER, folder);
+
+        final String dylibEnding = "." + VSysUtil.getPlatformSpecificLibraryEnding();
+
+        Collection<File> dynamicLibraries = new ArrayList<File>();
+
+        if (recursive) {
+            dynamicLibraries.addAll(
+                    IOUtil.listFiles(folder, new String[]{dylibEnding}));
+        } else {
+            File[] libFiles = folder.listFiles(new FilenameFilter() {
+
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(dylibEnding);
+                }
+            });
+            dynamicLibraries.addAll(Arrays.asList(libFiles));
+        }
+
+        System.out.println(">> loading native libraries:");
+
+        ArrayList<String> loadedLibraries = new ArrayList<String>();
+
+        int lastSize = -1;
+
+        while (loadedLibraries.size() > lastSize) {
+
+            lastSize = loadedLibraries.size();
+
+            for (File f : dynamicLibraries) {
+
+                String libName = f.getAbsolutePath();
+
+                if (!loadedLibraries.contains(libName)) {
+                    System.out.println(" --> " + f.getName());
+                    try {
+                        System.load(libName);
+                        loadedLibraries.add(libName);
+                    } catch (Exception ex) {
+                        ex.printStackTrace(System.err);
+                    }
+                }
+            }
+        }
+
+        return loadedLibraries.size() == dynamicLibraries.size();
+    }
+
+    /**
      * Defines all native UG classes that are exported via the UG registry,
      * i.e., the equivalent Java wrapper classes.
+     *
      * @param aNativeClasses the nativeClasses to set
      */
 //    static void setNativeClasses(Class<?>[] nativeClasses) {
@@ -143,6 +213,9 @@ public class UG {
 //    }
     public static void connectToNativeUG(boolean loadNativeLib) {
         // initialize native ug libraries
+
+
+
         String pluginPath = getNativeLibFolder() + "/eu/mihosoft/vrl/natives/"
                 + VSysUtil.getPlatformSpecificPath() + "plugins".replace("/", File.separator);
 
@@ -151,7 +224,14 @@ public class UG {
         System.out.println(" --> UG: connecting to native ug.");
 
         if (loadNativeLib) {
-            System.loadLibrary("ug4");
+
+            File libFolder = new File(
+                    getNativeLibFolder() + "/eu/mihosoft/vrl/natives/"
+                    + VSysUtil.getPlatformSpecificPath());
+
+            loadNativeLibrariesInFolder(libFolder, false);
+
+
             libLoaded = true;
         }
 
@@ -223,16 +303,17 @@ public class UG {
     /**
      * Tries to find a compatible api in the class path. Compatibility is
      * defined as equal svn revision and equal compile date.
+     *
      * @param ug ug instance used to check for compatibility.
-     * @return a compatible api class object or <code>null</code> if no such
-     * api could be found
+     * @return a compatible api class object or
+     * <code>null</code> if no such api could be found
      */
     private static Class<?> findCompatibleAPI(UG ug) {
         try {
             ClassLoader cl = null;
             try {
                 cl = new URLClassLoader(
-                        new URL[]{new File(Constants.PLUGIN_DIR
+                        new URL[]{new File(eu.mihosoft.vrl.system.Constants.PLUGIN_DIR
                             + "/" + Compiler.API_JAR_NAME).toURI().toURL()});
             } catch (MalformedURLException ex) {
                 Logger.getLogger(VRL.class.getName()).
@@ -242,7 +323,7 @@ public class UG {
             if (cl == null) {
                 System.err.println("Classloader not found: This should never"
                         + " happen. Please "
-                        + Constants.WRITE_VRL_BUG_REPORT_PLAIN);
+                        + eu.mihosoft.vrl.system.Constants.WRITE_VRL_BUG_REPORT_PLAIN);
 
                 return null;
             }
@@ -266,7 +347,7 @@ public class UG {
                     + "API found\n"
                     + " --> svn: present=" + apiSvn + "\n"
                     + " --> date: present=" + apiDate + "\n"
-                    + " --> location: " + VJarUtil.getClassLocation(cls)) );
+                    + " --> location: " + VJarUtil.getClassLocation(cls)));
 
             SplashScreenGenerator.printBootMessage(">> UG: API found");
 
@@ -300,6 +381,7 @@ public class UG {
     /**
      * Returns the api classes defined in the jar-file the specified class
      * object is loaded from.
+     *
      * @param cls api class
      * @return the api classes
      */
@@ -340,18 +422,14 @@ public class UG {
     }
 
     /**
-     * <p>
-     * Returns the instance of this singleton. If it does not exist it will be
-     * created.
-     * <p>
-     * <p>
-     * <b>Note:</b> this singleton must not be loaded by more than one
-     * classloader per JVM! Although this is no problem for Java classes it
-     * does not work for native libraries. Unfortunately we cannot provide an
-     * acceptable workaround. Thus, it is recommended to use this method from
-     * a valid VRL plugin only. The VRL plugin system is aware of this problem
-     * and handles it correctly.
-     * </p>
+     * <p> Returns the instance of this singleton. If it does not exist it will
+     * be created. <p> <p> <b>Note:</b> this singleton must not be loaded by
+     * more than one classloader per JVM! Although this is no problem for Java
+     * classes it does not work for native libraries. Unfortunately we cannot
+     * provide an acceptable workaround. Thus, it is recommended to use this
+     * method from a valid VRL plugin only. The VRL plugin system is aware of
+     * this problem and handles it correctly. </p>
+     *
      * @param canvas VRL canvas that shall be used to visualize ug classes
      * @return the instance of this singleton
      */
@@ -372,23 +450,15 @@ public class UG {
     }
 
     /**
-     * <p>
-     * Returns the instance of this singleton.
-     * </p>
-     * <p>
-     * <b>Note:</b>If message
-     * logging shall be used, please assign a visible canvas. For this
-     * {@link #getInstance(eu.mihosoft.vrl.reflection.VisualCanvas)  }
-     * can be used.
-     * </p>
-     * <p>
-     * <b>Note:</b> this singleton must not be loaded by more than one
-     * classloader per JVM if native libs have been loaded! Although this is
-     * no problem for Java classes, it
-     * does not work for native libraries. Unfortunately, we cannot provide an
-     * acceptable workaround. Thus, it is recommended to use this method from
-     * a valid VRL plugin only. The VRL plugin system is aware of this problem
-     * and handles it correctly.
+     * <p> Returns the instance of this singleton. </p> <p> <b>Note:</b>If
+     * message logging shall be used, please assign a visible canvas. For this
+     * {@link #getInstance(eu.mihosoft.vrl.reflection.VisualCanvas) }
+     * can be used. </p> <p> <b>Note:</b> this singleton must not be loaded by
+     * more than one classloader per JVM if native libs have been loaded!
+     * Although this is no problem for Java classes, it does not work for native
+     * libraries. Unfortunately, we cannot provide an acceptable workaround.
+     * Thus, it is recommended to use this method from a valid VRL plugin only.
+     * The VRL plugin system is aware of this problem and handles it correctly.
      * </p>
      */
     public static UG getInstance() {
@@ -397,6 +467,7 @@ public class UG {
 
     /**
      * Returns the VRL canvas that is used to visualize the UG classes.
+     *
      * @return the VRL canvas that is used to visualize the UG classes
      */
     public VisualCanvas getMainCanvas() {
@@ -404,12 +475,10 @@ public class UG {
     }
 
     /**
-     * <p>
-     * The VRL canvas that shall be used to visualize the UG classes
+     * <p> The VRL canvas that shall be used to visualize the UG classes </p>
+     * <p> <b>Notice:</b> as a side effect this method starts UG message logging
      * </p>
-     * <p>
-     * <b>Notice:</b> as a side effect this method starts UG message logging
-     * </p>
+     *
      * @param mainCanvas the VRL canvas to set
      */
     public void setMainCanvas(VisualCanvas mainCanvas) {
