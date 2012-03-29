10edfe12c6d94072a9daab1b826e403458f6a0c1
Christian Poliwoda <christian.poliwoda@gcsc.uni-frankfurt.de>
2012-03-29 14:21:19 +0200

    ! VersionInfo / Description ( Name & Number ) changed / adapted for VRL-UG and VRL-UG-API

--- VRL-UG/src/edu/gcsc/vrl/ug/Constants.java
+++ VRL-UG/src/edu/gcsc/vrl/ug/Constants.java
@@ -11,12 +11,17 @@ import eu.mihosoft.vrl.system.PluginIdentifier;
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
 public class Constants {
+    
+    public final static String PLUGIN_NAME = "VRL-UG4";
+    public final static String PLUGIN_NAME_API = "VRL-UG4-API";
+
+            
     public static final PluginIdentifier PLUGIN_IDENTIFIER =
-            new PluginIdentifier("UG4", "0.1.1");//+"."+Math.abs((int)System.currentTimeMillis()));//"0.1.1."+System.currentTimeMillis() did not work
+            new PluginIdentifier(PLUGIN_NAME, "0.2");//+"."+Math.abs((int)System.currentTimeMillis()));//"0.2."+System.currentTimeMillis() did not work
     
     
     public final static String JAR_PATH_KEY = "jarPath";
     public final static String PATH_TO_LOCAL_SERVER_FOLDER_KEY = "localServerFolder";
     public final static String REMOTETYPE_KEY = "remoteType";
     public final static String SERVER_SUFFIX = "-server";
-}
+    }
