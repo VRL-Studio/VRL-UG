6cee558b2010bb1007f0e0875faf00f831958f2c
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-08-31 17:53:17 +0200

    Better output of Error Message when Compilation failed in UserDataCompiler

--- VRL-UG/src/edu/gcsc/vrl/ug/UserDataCompiler.java
+++ VRL-UG/src/edu/gcsc/vrl/ug/UserDataCompiler.java
@@ -6,7 +6,11 @@ package edu.gcsc.vrl.ug;
 
 import eu.mihosoft.vrl.lang.InstanceCreator;
 import eu.mihosoft.vrl.lang.groovy.GroovyCompiler;
+import eu.mihosoft.vrl.system.VRL;
+import eu.mihosoft.vrl.visual.MessageType;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -30,11 +34,126 @@ public class UserDataCompiler {
     public static Object compile(String s, int dim) {
         GroovyCompiler c = new GroovyCompiler();
         c.addImport("import " + UserDataCompiler.PACKAGE_NAME + ".*;");
+        c.setCatchCompileException(false);
+//        c.setMainCanvas(VRL.getCurrentProjectController().getCurrentCanvas());
         InstanceCreator creator = new InstanceCreator();
-        Object result = creator.newInstance(c.compile(s, null));
-
+        
+        Object result = null;
+        try{
+            result = creator.newInstance(c.compile(s, null));
+        } catch (Exception ex) {
+                String msg = adaptErrorMessage(ex.getMessage());
+                
+                throw new RuntimeException(msg);
+                
+                
+//                VRL.getCurrentProjectController().getCurrentCanvas().getMessageBox().
+//                addMessage("Can't compile code:",
+//                msg, MessageType.ERROR);
+        }
+        
         return result;
     }
+
+    /**
+     * Returns the line number where the error occured.
+     * @param message the error message from the compiler
+     * @return the line number
+     */
+    public static int getErrorLine(String message) {
+        Pattern linePattern =
+                Pattern.compile(" @ line \\d+");
+
+        Matcher matcher = linePattern.matcher(message);
+
+        int line = 0;
+
+        if (matcher.find()) {
+            String lineString = matcher.group();
+
+            Pattern numberPattern =
+                    Pattern.compile("\\d+");
+
+            matcher = numberPattern.matcher(lineString);
+
+            if (matcher.find()) {
+
+                line = new Integer(matcher.group());
+            }
+        }
+        return line;
+    }
+
+    /**
+     * Returns the column number where the error occured.
+     * @param message the error message from the compiler
+     * @return the column number
+     */
+    public static int getErrorColumn(String message) {
+        Pattern linePattern =
+                Pattern.compile(", column \\d+");
+
+        Matcher matcher = linePattern.matcher(message);
+
+        int line = 0;
+
+        if (matcher.find()) {
+            String lineString = matcher.group();
+
+            Pattern numberPattern =
+                    Pattern.compile("\\d+");
+
+            matcher = numberPattern.matcher(lineString);
+
+            if (matcher.find()) {
+
+                line = new Integer(matcher.group());
+            }
+        }
+        return line;
+    }
+
+    /**
+     * Adapts an error message.
+     * @param message the message
+     * @return adapted error message
+     */
+    public static String adaptErrorMessage(String message) {
+
+        System.out.println("Error MESSAGE IN: \n---\n"+message+"\n---\n");
+        
+        int line = getErrorLine(message);
+        int column = getErrorColumn(message);
+
+        message = message.replaceFirst(
+                "startup failed:",
+                "");
+
+        message = message.replaceAll(
+                "script\\d+.groovy: \\d+:",
+                "");
+
+        message = message.replaceAll(
+                "@ line \\d+, column \\d+.",
+                "");
+
+        message = message.replaceAll(
+                "\\d+ errors",
+                "");
+
+        message = message.replaceAll(
+                "\\d+ error",
+                "");
+
+
+        System.out.println("Error MESSAGE OUT: \n---\n"+message+"\n---\n");
+
+        return "<br>@ line " + (line-5) + ", column " + column + ":<br>"
+                + "<pre><code>"
+                + message
+                + "</code></pre>";
+
+           }
 
     /**
      * 
@@ -59,7 +178,7 @@ public class UserDataCompiler {
         }
         
         // additional subset index parameter (always last param by definition)
-        paramString += "int " + paramNames.get(paramNames.size()-1) + " = _si;";
+        paramString += "int " + paramNames.get(paramNames.size()-1) + " = _si;\n";
 
         String text = "package " + PACKAGE_NAME+";\n"
                 + "import static java.lang.Math.*;\n"
