08e6a4558605d90c59296c1565ba0a4f391dbe8e
Michael Hoffer <info@michaelhoffer.de>
2015-06-18 12:04:11 +0200

    improve unsupported type handling.
    - INVALID_TYPE class is used to indicate broken type information without confusing the groovy compiler

---
+++ VRL-UG/src/edu/gcsc/vrl/ug/INVALID_TYPE.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.gcsc.vrl.ug;
+
+/**
+ * Marker interface. This type is only used to mark type information as invalid.
+ * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
+ */
+public final class INVALID_TYPE {
+    
+}
