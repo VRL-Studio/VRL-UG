5cf0544551bf83cb1d3aa0cd33fb4cfbdb750475
Michael Hoffer <info@michaelhoffer.de>
2015-06-29 15:21:12 +0200

    code cleanup and documentation fix

--- VRL-UG/src/edu/gcsc/vrl/ug/UGAPIClassCode.java
+++ VRL-UG/src/edu/gcsc/vrl/ug/UGAPIClassCode.java
@@ -6,7 +6,6 @@ package edu.gcsc.vrl.ug;
 
 import eu.mihosoft.vrl.lang.CodeBuilder;
 import eu.mihosoft.vrl.lang.VLangUtils;
-import java.util.ArrayList;
 
 /**
  *
@@ -17,60 +16,44 @@ public class UGAPIClassCode implements CodeElement {
     public UGAPIClassCode() {
     }
 
+    @Override
     public CodeBuilder build(CodeBuilder builder) {
         builder.addLine("class UGAPI {").
                 incIndentation().
-
                 // get svn revision
                 addLine("public static java.lang.String getSvnRevision() {").
                 incIndentation().
                 addLine("return \"" + VLangUtils.addEscapesToCode(UG.getInstance().getSvnRevision()) + "\";").
                 decIndentation().
                 addLine("}").
-                
                 // get compile date
                 addLine("public static java.lang.String getCompileDate() {").
                 incIndentation().
                 addLine("return \"" + VLangUtils.addEscapesToCode(UG.getInstance().getCompileDate()) + "\";").
                 decIndentation().
                 addLine("}").
-                
                 // get description
                 addLine("public static java.lang.String getDescription() {").
                 incIndentation().
                 addLine("return \"" + VLangUtils.addEscapesToCode(UG.getInstance().getDescription())
-                + "<br><br><b>Authors:</b><br><br>"
-                + VLangUtils.addEscapesToCode(UG.getInstance().getAuthors()).replace("\n", "<br>") + "\";").
+                        + "<br><br><b>Authors:</b><br><br>"
+                        + VLangUtils.addEscapesToCode(UG.getInstance().getAuthors()).replace("\n", "<br>") + "\";").
                 decIndentation().
                 addLine("}").
-        
                 // get ug version
                 addLine("public static java.lang.String getVersion() {").
                 incIndentation().
-                addLine("return \"" + getVersion() +"\";").
+                addLine("return \"" + getVersion() + "\";").
                 decIndentation().
                 addLine("}").
-                
                 decIndentation().
                 addLine("}");
-
-
 
         return builder;
 
     }
 
     private String getVersion() {
-        // TODO: remove this method if ug4.0.1 is released!
-        
-        String version = "4.0.0"; // fixme !
-        
-        try {
-            return VLangUtils.addEscapesToCode(UG.getInstance().getUGVersion());
-        }catch(Throwable tr) {
-            //
-        }
-        
-        return VLangUtils.addEscapesToCode(version);
+        return VLangUtils.addEscapesToCode(UG.getInstance().getUGVersion());
     }
 }
