1295bd6afafbaaba649b0e7f6e50546d030c9ed5
Michael Hoffer <info@michaelhoffer.de>
2015-06-29 12:45:15 +0200

    build.properties provides plugin name and version properties

--- VRL-UG/src/edu/gcsc/vrl/ug/Constants.java
+++ VRL-UG/src/edu/gcsc/vrl/ug/Constants.java
@@ -11,19 +11,21 @@ import eu.mihosoft.vrl.system.PluginIdentifier;
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
 public class Constants {
-    
-    public final static String PLUGIN_NAME = "VRL-UG";
-    public final static String PLUGIN_NAME_API = "VRL-UG-API";
 
-            
-    public static final PluginIdentifier PLUGIN_IDENTIFIER =
-            new PluginIdentifier(PLUGIN_NAME, "0.2");//+"."+Math.abs((int)System.currentTimeMillis()));//"0.2."+System.currentTimeMillis() did not work
-    
-    
+    public final static String PLUGIN_NAME = /*<VRL_UG_PLUGIN_NAME>*/"VRL-UG"/*</VRL_UG_PLUGIN_NAME>*/;
+    public final static String PLUGIN_NAME_API = /*<VRL_UG_PLUGIN_API_NAME>*/"VRL-UG-API"/*</VRL_UG_PLUGIN_API_NAME>*/;
+    public final static String PLUGIN_VERSION = /*<VRL_UG_PLUGIN_VERSION>*/"0.2.1"/*</VRL_UG_PLUGIN_VERSION>*/;
+    public final static String PLUGIN_VERSION_API = /*<VRL_UG_PLUGIN_API_VERSION>*/"0.2.1"/*</VRL_UG_PLUGIN_API_VERSION>*/;
+
+    public static final PluginIdentifier PLUGIN_IDENTIFIER
+            = new PluginIdentifier(PLUGIN_NAME, PLUGIN_VERSION);
+    public static final PluginIdentifier API_PLUGIN_IDENTIFIER
+            = new PluginIdentifier(PLUGIN_NAME_API, PLUGIN_VERSION_API);
+
     public final static String JAR_PATH_KEY = "jarPath";
     public final static String PATH_TO_LOCAL_SERVER_FOLDER_KEY = "localServerFolder";
     public final static String REMOTETYPE_KEY = "remoteType";
     public final static String SERVER_SUFFIX = "-server";
 
     public final static String DETAILED_EXCEPTION_KEY = "detailedException";
-    }
+}
